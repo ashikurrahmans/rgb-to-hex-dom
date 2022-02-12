379ed1008f8adc068c387748b4e784fb9f513cfb
HackBomm <hackbomm.net@gmail.com>
2022-02-12 15:14:50 +0600

    RGB color to Hex

--- custom.js
+++ custom.js
@@ -7,11 +7,12 @@ function changeBackground(){
     const red = Math.floor(Math.random() * 255)
     
     const color = `rgb(${red},${green},${blue})`
+     console.log(color)
     const bluehexaConvert = blue.toString(16)
     const greenhexaConvert = green.toString(16)
     const redhexaConvert = red.toString(16)
-    const hexaConvert = bluehexaConvert + greenhexaConvert + redhexaConvert
-    // const changeColor = document.getElementsByTagName('body')[0].style.background = color;
+    const hexaConvert = redhexaConvert + greenhexaConvert + bluehexaConvert;
+    document.getElementsByTagName('body')[0].style.background = color;
     const hexColor = document.getElementById('title').innerText = `Hex Color Code : #${hexaConvert}`
 
     return hexColor;
